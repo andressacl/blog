f0f6550d7da8c641d1d8f8c3f2875cb2631f7cd4
Andressa Colaço <andressacolaco@usp.br>
2023-07-27 00:59:03 -0300

    Update site

--- site.config.js
+++ site.config.js
@@ -23,14 +23,14 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://blog-andressacl.vercel.app/",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'pt-BR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: process.env.5479f238c3f2403da063f212a0a2da54,
   },
 
   // plugin configuration (optional)
