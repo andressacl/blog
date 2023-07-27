c7304dc9d92a0c39e3718dc30ad363e105864246
Andressa Colaço <andressacolaco@usp.br>
2023-07-26 23:56:19 -0300

    Update basic info

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Andressa",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Física Computacional",
+    bio: "",
+    email: "andressacolaco@usp.br",
+    linkedin: "",
+    github: "andressacl",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+    
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "andressacl",
+    description: "Blog voltado a mini-projetos feitos na graduação.",
   },
 
   // CONFIG configration (required)
@@ -47,7 +46,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
